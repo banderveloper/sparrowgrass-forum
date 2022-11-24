12e7b7b624a7ab253c9af09209d7f77cf8489efa
kpssnik <kpssdeveloper@gmail.com>
2022-11-24 21:39:43 +0200

    Injected repositories

--- SparrowgrassForum.Server/Program.cs
+++ SparrowgrassForum.Server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SparrowgrassForum.Server.Domain;
+using SparrowgrassForum.Server.Domain.Repositories.Abstract;
+using SparrowgrassForum.Server.Domain.Repositories.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -8,10 +10,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
 });
 
-
+builder.Services.AddSingleton<IUserRepository, EfUserRepository>();
+builder.Services.AddSingleton<IEatRecordRepository, EfEatRecordRepository>();
 
 var app = builder.Build();
 app.MapControllers();
-
 
 app.Run();
