92e4179d925f7ef30fbb4c8bb37960b5596c0c2c
kpssnik <kpssdeveloper@gmail.com>
2022-11-25 03:17:47 +0200

    Service setup moved to separate class, main became clearer

--- SparrowgrassForum.Server/Program.cs
+++ SparrowgrassForum.Server/Program.cs
@@ -1,25 +1,7 @@
-using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
-using SparrowgrassForum.Server.Domain;
-using SparrowgrassForum.Server.Domain.Repositories;
-using SparrowgrassForum.Server.Domain.Repositories.Abstract;
-using SparrowgrassForum.Server.Domain.Repositories.EntityFramework;
+using SparrowgrassForum.Server;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers().AddJsonOptions(options => {
-    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-});
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
-});
-
-builder.Services.AddScoped<IUserRepository, EfUserRepository>();
-builder.Services.AddScoped<IEatRecordRepository, EfEatRecordRepository>();
-builder.Services.AddScoped<DataManager>();
+new ServiceSetup(builder).Init();
 
 var app = builder.Build();
 app.MapControllers();
---
+++ SparrowgrassForum.Server/ServiceSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using SparrowgrassForum.Server.Domain;
+using SparrowgrassForum.Server.Domain.Repositories;
+using SparrowgrassForum.Server.Domain.Repositories.Abstract;
+using SparrowgrassForum.Server.Domain.Repositories.EntityFramework;
+
+namespace SparrowgrassForum.Server;
+
+public class ServiceSetup
+{
+    private readonly WebApplicationBuilder _builder;
+
+    public ServiceSetup(WebApplicationBuilder builder)
+    {
+        this._builder = builder;
+    }
+
+    public void Init()
+    {
+        _builder.Services.AddControllers().AddJsonOptions(options => {
+            options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+
+        _builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(_builder.Configuration.GetConnectionString("SqlServer"));
+        });
+
+        _builder.Services.AddScoped<IUserRepository, EfUserRepository>();
+        _builder.Services.AddScoped<IEatRecordRepository, EfEatRecordRepository>();
+        _builder.Services.AddScoped<DataManager>();
+    }
+}
